692419b5380298c937d23444f10be0a98c79ecf0
Tamara Rivera F <tamita.rivera@gmail.com>
2012-06-05 04:18:13 -0400

    Prototipo de primer entregable

---
+++ spikes/entregable/DLI3D.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtGui
+
+
+class Application(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Application, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):               
+        
+        self.resize(800, 600)
+        self.center()
+        
+        self.openSTLButton = QtGui.QPushButton('Open STL')
+        self.openSTLButton.clicked.connect(self.showOpenFile)
+        
+        self.createAnimationButton = QtGui.QPushButton('Create Animation')
+        self.createAnimationButton.setEnabled(False)
+##        createAnimationButton.clicked.connect(self.makeAnimation)
+
+        welcome = QtGui.QLabel('<h1>Welcome</h1>', self)
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+        grid.addWidget(QtGui.QLabel('<h3>Please, select the STL file you want to print</h3>'), 1,0)
+        grid.addWidget(QtGui.QLabel('<h3>When you are ready press Print to start printing</h3>'), 2,0)
+        grid.addWidget(self.openSTLButton, 1, 2)
+        grid.addWidget(self.createAnimationButton, 2, 2)
+        
+        self.setLayout(grid)
+        self.setWindowTitle('DLI3D')    
+        self.show()
+        
+    def showOpenFile(self):
+        fileName = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
+                '/home', self.tr("STL Files (*.stl)"))
+        if fileName:
+            openFile = open(fileName, 'r')
+            ##createImages(openFile)
+            self.createAnimationButton.setEnabled(True)
+                                                     
+    ##def createImages(self, file):
+        ##insertar el codigo para crear las imagenes
+    
+##    def makeAnimation(self):
+        ##insertar el codigo que crea la animación
+    def center(self):
+        
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+        
+        
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Application()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
