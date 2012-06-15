f1d6dce16c67530ae30fef0f27483267662dca82
Álvaro Faúndez <alvaro@faundez.net>
2012-06-15 11:27:31 -0400

    ADD: tests with mock

--- lib/example.py
+++ lib/example.py
@@ -1,2 +1,11 @@
+class NotImplementedClass:
+    pass
+
+
 def giveMeSomething():
     return ""
+
+
+def methodWithNotImplementedClass():
+    bird = NotImplementedClass()
+    bird.notImplementedMethod()
