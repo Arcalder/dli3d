9fb1767d1f9c28c2d1f065057b8c834074709b7f
Tamara Rivera F <tamita.rivera@gmail.com>
2012-06-15 12:14:53 -0400

    Se deja en src una nueva version de la interfaz y se crea un test para probar la funcionalidad de cortar el stl

---
+++ lib/slices.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+def createSlices(height,output,path,step,layer_thickness):
+	os.system("slice "+path+" -z0,"+str(height)+","+str(step)+" -l "+str(layer_thickness)+" -o "+output)
+
+
+#height = raw_input("Enter height: ")
+#output = raw_input("Enter output: ")
+#path = raw_input("Enter stl absolute path: ")
+#step = raw_input("Enter step: ")
+#layer_thickness = raw_input("Enter layer thickness: ")
+#createSlices(height,output,path,step,layer_thickness)
