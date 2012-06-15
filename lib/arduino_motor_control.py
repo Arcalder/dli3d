a1443a175505363000f586b76215db819960bcc7
Eduardo Escobar <escobar.edo@gmail.com>
2012-06-15 13:11:12 -0400

    Tests de ArduinoMotorControl incompleto

--- lib/arduino_motor_control.py
+++ lib/arduino_motor_control.py
@@ -9,28 +9,28 @@ import serial
 
 class ArduinoMotorControl():
     def __init__(self, arduino_port):
-        ser = serial.Serial(arduino_port, 9600)
+        self.ser = serial.Serial(arduino_port, 9600)
 
     def move_up(self):
         try:
-           ser.write(".")
+           self.ser.write(".")
         except Exception:
            print str(Exception)
     
     def move_down(self):
         try:
-           ser.write("-")
+           self.ser.write("-")
         except Exception:
            print str(Exception)
            
     def move_up_steps(self, steps):
         try:
-           ser.write(steps+"u")
+           self.ser.write(steps+"u")
         except Exception:
            print str(Exception)
            
     def move_down_steps(self, steps):
         try:
-           ser.write(steps+"d")
+           self.ser.write(steps+"d")
         except Exception:
            print str(Exception)
--- tests/arduino_motor_control_test.py
+++ tests/arduino_motor_control_test.py
@@ -3,14 +3,22 @@ import os
 sys.path.append(os.path.join(os.getcwd(), 'lib'))
 
 import unittest
+from mock import patch
+
 import serial
 from arduino_motor_control import *
 
 class TestArduinoMotorControl(unittest.TestCase):
 
-	def testInit(self):
+	@patch('serial.Serial')
+	def testInit(self, mock_arduino_motor_control_class):
 		#arrange
 		PORT = 'COM6'
 		#act
 		arduino = ArduinoMotorControl(PORT)
+		#assert
+		mock_arduino_motor_control_class.assert_called_with(PORT,9600)
 		self.assertTrue(arduino is not None)
+		#print type(arduino.ser)
+		#print type(mock_arduino_motor_control_class)
+		#TODO:self.assertEqual(arduino.ser.__class__,mock_arduino_motor_control_class.__class__)
