5ceae23d910f14a6ba378f4604fa909fc47003db
Eduardo Escobar <escobar.edo@gmail.com>
2012-06-15 14:49:25 -0400

    Agregados comandos para mover motor Servo desde la libreria de nuestra aplicacion

--- lib/arduino_motor_control.py
+++ lib/arduino_motor_control.py
@@ -1,36 +1,48 @@
-#-------------------------------------------------------------------------------
+﻿#-------------------------------------------------------------------------------
 # Name:        ArduinoControl
 #
-# Author:      Eduardo Escobar
+# Author:      Eduardo Escobar - Roberto Riquelme - Alonso Gaete
 #
 # Created:     18-05-2012
 #-------------------------------------------------------------------------------
 import serial
 
 class ArduinoMotorControl():
-    def __init__(self, arduino_port):
-        self.ser = serial.Serial(arduino_port, 9600)
+	def __init__(self, arduino_port):
+		self.ser = serial.Serial(arduino_port, 9600)
 
-    def move_up(self):
-        try:
-           self.ser.write(".")
-        except Exception:
-           print str(Exception)
-    
-    def move_down(self):
-        try:
-           self.ser.write("-")
-        except Exception:
-           print str(Exception)
-           
-    def move_up_steps(self, steps):
-        try:
-           self.ser.write(steps+"u")
-        except Exception:
-           print str(Exception)
-           
-    def move_down_steps(self, steps):
-        try:
-           self.ser.write(steps+"d")
-        except Exception:
-           print str(Exception)
+	def move_up(self):
+		try:
+			self.ser.write(".")
+		except Exception:
+			print str(Exception)
+
+	def move_down(self):
+		try:
+			self.ser.write("-")
+		except Exception:
+			print str(Exception)
+
+	def move_up_steps(self, steps):
+		try:
+			self.ser.write(str(steps)+"u")
+		except Exception:
+			print str(Exception)
+
+	def move_down_steps(self, steps):
+		try:
+			self.ser.write(str(steps)+"d")
+		except Exception:
+			print str(Exception)
+
+	def close_valve(self):
+		try:
+			self.ser.write("c")
+		except Exception:
+			print str(Exception)
+
+	def open_valve(self):
+		try:
+			self.ser.write("o")
+		except Exception:
+			print str(Exception)
