ecf689e5579a2931c3e550407f81a9fec16c0483
Leonardo Rojas <lerojasrojas@gmail.com>
2012-06-20 20:03:18 -0400

    Version entregable de DLI3D.py funcional, se generan las imagenes en el directorio seleccionado, falta integracion con generacion de video

--- spikes/entregable/DLI3D.py
+++ spikes/entregable/DLI3D.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 from PyQt4 import QtGui
-from slices import createSlices
+#sys.path.append(os.path.join(os.getcwd(), '..', 'lib'))
+
+from slices import *
 
 class Application(QtGui.QWidget):
     
@@ -11,58 +14,88 @@ class Application(QtGui.QWidget):
         super(Application, self).__init__()
         
         self.initUI()
-        
-    def initUI(self):               
+    def checker(self):
+        if self.heightInput.text()!='' and self.stepInput.text()!='' and self.layerInput.text()!='':
+            self.openSTLButton.setEnabled(True)
+        else:
+            self.openSTLButton.setEnabled(False)
+    
+    def initUI(self):
         
         self.resize(800, 600)
         self.center()
         
         self.heightInput = QtGui.QLineEdit()
-        self.outputInput = QtGui.QLineEdit()
         self.stepInput = QtGui.QLineEdit()
         self.layerInput = QtGui.QLineEdit()
         
+#        self.layerInput.textChanged('',self.checker())
+#        self.layerInput.textChanged('',self.checker())
+#        self.layerInput.textChanged(self.checker())
+        
+
+        
+        self.dirLabel = QtGui.QLabel('Output Directory')
+        self.stlLabel = QtGui.QLabel('Please, select the STL file you want to print')
+        
+        self.outputFileName = ''
+        self.outputButton = QtGui.QPushButton('Open Folder')
+        self.outputButton.clicked.connect(self.showOpenFileOutput)
+         
         self.openSTLButton = QtGui.QPushButton('Open STL')
         self.openSTLButton.clicked.connect(self.showOpenFile)
+        self.openSTLButton.setEnabled(False)
         
         self.createAnimationButton = QtGui.QPushButton('Print')
         self.createAnimationButton.setEnabled(False)
-##        createAnimationButton.clicked.connect(self.makeAnimation)
+## createAnimationButton.clicked.connect(self.makeAnimation)
 
-        welcome = QtGui.QLabel('<h1>Welcome</h1>', self)
-        self.printLabel = QtGui.QLabel('<h3>When you are ready press Print to start printing</h3>')
+        #welcome = QtGui.QLabel('<h1>Welcome</h1>', self)
+        self.printLabel = QtGui.QLabel('When you are ready press Print to start printing')
+        
         grid = QtGui.QGridLayout()
         grid.setSpacing(10)
         
-        grid.addWidget(QtGui.QLabel('<h2>Height</h2>'), 0 , 0)
-        grid.addWidget(self.heightInput, 0, 1)
-        grid.addWidget(QtGui.QLabel('<h2>Output Directory</h2>'), 1 , 0)
-        grid.addWidget(self.outputInput, 1, 1)
-        grid.addWidget(QtGui.QLabel('<h2>Step</h2>'), 1 , 0)
-        grid.addWidget(self.stepInput, 1, 1)
-        grid.addWidget(QtGui.QLabel('<h2>Layer Thickness</h2>'), 1 , 0)
-        grid.addWidget(self.layerInput, 1, 1)
+        grid.addWidget(QtGui.QLabel('Height'), 0 , 0)
+        grid.addWidget(self.heightInput, 0, 2)
+        grid.addWidget(QtGui.QLabel('Step'), 1 , 0)
+        grid.addWidget(self.stepInput, 1, 2)
+        grid.addWidget(QtGui.QLabel('Layer Thickness'), 2 , 0)
+        grid.addWidget(self.layerInput, 2, 2)
+        grid.addWidget(self.dirLabel, 3 , 0)
+        grid.addWidget(self.outputButton, 3, 2)
         
-        grid.addWidget(QtGui.QLabel('<h3>Please, select the STL file you want to print</h3>'), 1,0)
-        grid.addWidget(self.printLabel, 2,0)
-        grid.addWidget(self.openSTLButton, 1, 2)
-        grid.addWidget(self.createAnimationButton, 2, 2)
+        grid.addWidget(self.stlLabel, 4,0)
+        grid.addWidget(self.printLabel, 5,0)
+        grid.addWidget(QtGui.QLabel(''), 4 , 1)
+        grid.addWidget(self.openSTLButton, 4, 2)
+        grid.addWidget(self.createAnimationButton, 5, 2)
         
         self.setLayout(grid)
-        self.setWindowTitle('DLI3D')    
+        self.setWindowTitle('DLI3D')
         self.show()
         
+    def createImages(self,fileName):
+        createSlices(self.heightInput.text(), str(self.outputFileName)+'.jpg', str(fileName), self.stepInput.text(), self.layerInput.text())
+    
     def showOpenFile(self):
-        fileName = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
+        fileName = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
                 '/home', self.tr("STL Files (*.stl)"))
         if fileName:
-            createImages(fileName)
+            self.stlLabel.setText('Selected STL file'+fileName)
+            self.createImages(fileName)
             self.createAnimationButton.setEnabled(True)
-            self.printLabel.setText('<h3>When you are ready press Print to start printing '  + fileName + '</h3>')                                        
-    def createImages(self, fileName):
-        createSlices(self.heightInput.text, self.outputInput.text, fileName, self.stepInput.text, self.layerInput.text)
+            self.printLabel.setText('When you are ready press Print to start printing ' + fileName)
     
-##    def makeAnimation(self):
+    def showOpenFileOutput(self):
+        dirName = QtGui.QFileDialog.getExistingDirectory(self, 'Open directory',
+                '/home')
+        if dirName:
+         self.outputFileName = dirName
+         self.dirLabel.setText("Selected directory: " + dirName)
+         self.checker()
+    
+## def makeAnimation(self):
         ##insertar el codigo que crea la animación
     def center(self):
         
---
+++ spikes/entregable/slices.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+def createSlices(height,output,path,step,layer_thickness):
+	#height=5
+	#output="trololo.jpg"
+	#path=os.path.abspath("C:/Users/Leonardo/Documents/Helix.stl")
+	#print path
+	#step=0.5
+	#layer_thickness=10
+	print height
+	print output
+	print path
+	print step
+	print layer_thickness
+	dir=output[:(-len(output)+output.find('.'))]
+	new_output= output[len(output)-(output[::-1]).find('\\'):]
+	print new_output
+	print dir
+	if not (os.path.exists(dir)):
+		os.mkdir(dir)
+	print 'Antes del chdir'+os.getcwd()
+	os.chdir(dir)
+	print 'Despues del chdir'+os.getcwd()
+	#os.system("slice "+path+" -z0,"+str(height)+",0.5 -o "+output)
+	os.system("slice "+path+" -z0,"+str(height)+","+str(step)+" -l "+str(layer_thickness)+" -o "+new_output)
+
+
+#height = raw_input("Enter height: ")
+#output = raw_input("Enter output: ")
+#path = raw_input("Enter stl absolute path: ")
+#step = raw_input("Enter step: ")
+#layer_thickness = raw_input("Enter layer thickness: ")
+#createSlices(height,output,path,step,layer_thickness)
