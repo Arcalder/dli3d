4d606c028ca81a68f47683dfabaad3bc4bd7609b
Tamara Rivera F <tamita.rivera@gmail.com>
2012-07-06 13:04:14 -0400

    se agregan las licencias a los archivos

--- lib/volumeAcumulator.py
+++ lib/volumeAcumulator.py
@@ -1,3 +1,26 @@
+﻿#-------------------------------------------------------------------------------
+# This file is part of 'DlI3D'.
+# 
+# Copyright (C) 2012 by
+# Ariel Calderón, Cesar Campos, Eduardo Escobar, Alvaro Faundez, Alonso Gaete,
+# Felipe Gonzalez, Rodrigo Gonzalez, Roberto Riquelme, Tamara Rivera, 
+# Leonardo Rojas, Maximilian Santander
+# DlI3D: https://github.com/afaundez/dli3d
+# 
+# 'DlI3D' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with 'DlI3D'.  If not, see <http://www.gnu.org/licenses/>.
+#
+#-------------------------------------------------------------------------------
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from getVolumeFromPixels import countWhitePixels
