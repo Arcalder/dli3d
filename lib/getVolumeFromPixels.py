a0a4a7999f29c350bcd122cde1fc8a9d971c19d5
Tamara Rivera F <tamita.rivera@gmail.com>
2012-08-06 01:22:52 -0400

    Revert "Arreglado error de identacion"
    This reverts commit 6f3813e0df45094b9ce7758056312a0f1daebdff.

--- lib/getVolumeFromPixels.py
+++ lib/getVolumeFromPixels.py
@@ -7,6 +7,6 @@ def countWhitePixels(image):
     img_I = Image.open(str(image)).getdata()
     whitePixels = 0
     for pixel in list(img_I):
-       if  pixel == (255,255,255):
-           whitePixels += 1
-    return whitePixels
+            if  pixel == (255,255,255):
+                whitePixels += 1
+        return whitePixels
